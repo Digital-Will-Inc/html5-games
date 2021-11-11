12f9c45ca33201d3d0041682c2b874769f620cb5
WilliamAB <liams_1998@hotmail.com>
2021-11-11 21:29:36 +0200

    Minor refactor in BuildCreator.js

--- BuildCreator.js
+++ BuildCreator.js
@@ -32,44 +32,7 @@ function zipDirectory(source, out, gameName) {
             .on('error', err => reject(err))
             .pipe(stream);
 
-        stream.on('close', () => {
-            resolve();
-            console.log(`${gameName} created with ` + archive.pointer() + ' total bytes');
-        });
-
-        stream.on('error', err => reject(err));
+        gistream.on('error', err => reject(err));
         archive.finalize();
     });
 }
-
-
-// const output = fs.createWriteStream(OUTPUT_DIR);
-
-// output.on('close', function () {
-//     console.log(archive.pointer() + ' total bytes');
-//     console.log('archiver has been finalized and the output file descriptor has closed.');
-// });
-
-
-// archive.on('warning', function (err) {
-//     if (err.code === 'ENOENT') {
-//         // log warning
-//     } else {
-//         // throw error
-//         throw err;
-//     }
-// });
-
-// archive.on('error', function (err) {
-//     throw err;
-// });
-
-
-
-// const folder1 = __dirname + "/ColorMatch" + "/build/";
-
-
-// archive.directory(folder1, false);
-// archive.pipe(output);
-// archive.finalize();
-
