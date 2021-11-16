2e39f8b6573875b3e6676f7ea53501db335f9d35
WilliamAB <liams_1998@hotmail.com>
2021-11-16 23:04:27 +0200

    Add updated wortal ad to HexGL

---
+++ HexGL/Build/WortalAd.js
@@ -0,0 +1,38 @@
+var WORTAL_API_SCRIPT = document.createElement("script");
+WORTAL_API_SCRIPT.src = "https://html5gameportal.com/embeds/wortal-1.1.1.js";
+WORTAL_API_SCRIPT.type = 'text/javascript';
+WORTAL_API_SCRIPT.async = true;
+const head = document.getElementsByTagName("head");
+head[head.length - 1].appendChild(WORTAL_API_SCRIPT);
+
+const AdTypes = {
+    interstitailAd: 'start',
+    rewardedAd: 'reward',
+}
+
+window.addEventListener("load", () => {
+    window.initWortal(function () {
+        console.log("Wortal setup complete!");
+        setTimeout(() => {
+            CallAd(AdTypes.interstitailAd, "Interstitial Ad");
+        }, 100);
+    });
+});
+
+
+function CallAd(type, name) {
+    window.triggerWortalAd(type, name, {
+        beforeAd: function () {
+            console.log("Call beforeAd");
+        },
+        afterAd: function () {
+            console.log("Call afterAd");
+        },
+        adBreakDone: function () {
+            console.log("Call adBreakDone");
+        },
+        noShow: function () {
+
+        }
+    });
+}
