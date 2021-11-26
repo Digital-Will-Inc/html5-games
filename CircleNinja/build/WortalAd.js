a6459ed49c381acb4f6e982fc327734a559dda71
WilliamAB <liams_1998@hotmail.com>
2021-11-26 06:22:17 +0200

    Port CirleNinja

---
+++ CircleNinja/build/WortalAd.js
@@ -0,0 +1,49 @@
+var WORTAL_API_SCRIPT = document.createElement("script");
+WORTAL_API_SCRIPT.src = "https://html5gameportal.com/embeds/wortal-1.1.1.js";
+WORTAL_API_SCRIPT.type = 'text/javascript';
+WORTAL_API_SCRIPT.async = true;
+const head = document.getElementsByTagName("head");
+head[head.length - 1].appendChild(WORTAL_API_SCRIPT);
+
+const AdTypes = {
+    preroll: "preroll",
+    start: 'start',
+    pause: 'pause',
+    next: 'next',
+    browse: 'browse',
+    rewardedAd: 'reward',
+}
+
+window.addEventListener("load", () => {
+    window.initWortal(function () {
+        console.log("Wortal setup complete!");
+        wortalIsLoaded = true;
+        setTimeout(() => {
+            CallAd(AdTypes.start, "Interstitial Ad");
+        }, 100);
+    });
+});
+
+
+let wortalIsLoaded = false;
+function CallAd(type, name, beforeAd, afterAd, adBreakDone, noShow) {
+    if (wortalIsLoaded == false) return;
+    window.triggerWortalAd(type, name, {
+        beforeAd: function () {
+            console.log("Call beforeAd");
+            if (beforeAd) beforeAd();
+        },
+        afterAd: function () {
+            console.log("Call afterAd");
+            if (afterAd) afterAd();
+        },
+        adBreakDone: function () {
+            console.log("Call adBreakDone");
+            if (adBreakDone) adBreakDone();
+        },
+        noShow: function () {
+            console.log("Call noShow");
+            if (noShow) noShow();
+        }
+    });
+}
---
+++ CircleNinja/build/i18n.js
@@ -0,0 +1,18 @@
+const i18n = [
+    {
+        en: "Game Over",
+        ja: "ゲームオーバー"
+    },
+    {
+        en: "High Score",
+        ja: "ハイスコア",
+    },
+    {
+        en: "Score",
+        ja: "スコア",
+    },
+    {
+        en: "Tip: get the green ones!",
+        ja: "ヒント: グリーンのものを取る！",
+    }
+];
