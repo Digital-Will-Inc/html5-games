ecbde0e71bf5e217f0c5d2cc5f29a29baf93e202
Tim Dillon <tim@itsdits.com>
2022-09-23 19:32:06 +0900

    feat: wortal.js 1.3.0
    * Adds analytics API
    * Fixes noShow not called on Link

--- Utils/wortal.js
+++ Utils/wortal.js
@@ -13,8 +13,10 @@
  *  This is necessary to hide the game rendering while pre-roll ads are shown on certain platforms.
  *  You should not show the game canvas or play any audio until <code>hasPlayedPreroll == true</code>.
  *
- * @version 1.2.0
+ * @version 1.3.0
  */
+
+const GAME_NAME = "MyGame";
 
 const wortal = document.createElement('script');
 wortal.src = 'https://html5gameportal.com/embeds/wortal-1.2.0.js';
@@ -62,9 +64,14 @@ let hasPlayedPreroll = false;
 let linkInterstitialId = "";
 let linkRewardedId = "";
 
-init();
+let gameTimer = 0;
+let levelTimer = 0;
+let levelTimerHandle;
+let levelName = "";
 
-function init() {
+initWortalSdk();
+
+function initWortalSdk() {
     window.addEventListener('load', () => {
         window.initWortal(function () {
             console.log('[Wortal] Setup complete.');
@@ -109,6 +116,18 @@ function init() {
                 });
             }
         });
+
+        window.addEventListener('visibilitychange', () => {
+            if (document.visibilityState === "hidden") {
+                logGameEnd();
+            }
+        });
+
+        getIntlData()
+            .then(response => logGameStart(response))
+            .catch(() => logGameStart("Nulltherlands"));
+
+        console.log("[Wortal] Initialized");
     });
 }
 
@@ -163,6 +182,7 @@ function showInterstitial(type, description, callbacks) {
     }
     if (callbacks.noShow) {
         params.noShow = callbacks.noShow;
+        params.noBreak = callbacks.noShow;
     }
     if (callbacks.adBreakDone) {
         params.adBreakDone = callbacks.adBreakDone;
@@ -245,12 +265,166 @@ function showRewarded(description, callbacks) {
     }
     if (callbacks.noShow) {
         params.noShow = callbacks.noShow;
+        params.noBreak = callbacks.noShow;
     }
     if (callbacks.adBreakDone) {
         params.adBreakDone = callbacks.adBreakDone;
     }
 
     window.triggerWortalAd(Placement.REWARD, linkRewardedId, description, params);
+}
+
+/**
+ * Logs an event when the player begins the tutorial. Starts a timer that tracks how long the player spends in this
+ * level. Call logTutorialEnd() to get the value of the timer in the event.
+ */
+function logTutorialStart() {
+    logLevelStart("Tutorial");
+}
+
+/**
+ * Logs an event when the player finishes the tutorial. This can be used to determine how many players complete
+ * the tutorial vs skipping.
+ */
+function logTutorialEnd() {
+    logLevelEnd("Tutorial");
+}
+
+/**
+ * Logs an event for the player starting a level. This will trigger a level timer with the given level name. If
+ * logLevelEnd() is called with the same level name, it will record the time it took the player to finish the level.
+ * @param level {string} Level the player started.
+ * @return {number} ID of the timer being used to track the level time.
+ */
+function logLevelStart(level) {
+    if (levelTimerHandle != null) {
+        clearInterval(levelTimerHandle);
+        levelTimerHandle = null;
+    }
+    levelName = level;
+    levelTimer = 0;
+    levelTimerHandle = setInterval(() => levelTimer += 1, 1000);
+    logEvent("LevelStart", {
+        game: GAME_NAME,
+        level: level,
+    });
+}
+
+/**
+ * Logs an event for the player ending a level. Also logs how long it took the player to complete the level if
+ * logLevelStart() was called before this with the same level variable.
+ * @param level {string} Level the player played.
+ * @param wasCompleted {boolean} Did the player complete the level or not.
+ * @param score {string} Score the player achieved in the level.
+ */
+function logLevelEnd(level, wasCompleted = true, score = '0') {
+    if (levelTimerHandle != null) {
+        clearInterval(levelTimerHandle);
+        levelTimerHandle = null;
+    }
+    if (levelName !== level) {
+        levelTimer = 0;
+    }
+    logEvent("LevelEnd", {
+        game: GAME_NAME,
+        level: level,
+        time: levelTimer,
+        complete: wasCompleted,
+        score: score,
+    });
+    levelTimer = 0;
+}
+
+/**
+ * Logs an event when the player levels up.
+ * @param level {string} Level the player achieved.
+ */
+function logLevelUp(level) {
+    logEvent("LevelUp", {
+        game: GAME_NAME,
+        level: level,
+    });
+}
+
+/**
+ * Logs the players score. This can be used to record a high score or track scores across different points in the
+ * game to determine difficulty level or imbalances.
+ * @param {string} score Score the player achieved.
+ */
+function logScore(score) {
+    logEvent("PostScore", {
+        game: GAME_NAME,
+        score: score,
+    });
+}
+
+/**
+ * Logs a choice the player makes in the game. This can be useful for determining which characters are more
+ * popular, which paths are more commonly taken, etc. This can be a powerful tool for balancing the game and
+ * giving the players more of what they enjoy.
+ * @param {string} decision Decision the player was faced with. Ex: Character
+ * @param {string} choice Choice the player made. Ex: Blue Dog
+ */
+function logGameChoice(decision, choice) {
+    logEvent("GameChoice", {
+        game: GAME_NAME,
+        decision: decision,
+        choice: choice,
+    });
+}
+
+// This is automatically called when the Wortal SDK is initialized. It should not be called by the game.
+function logGameStart(country) {
+    const platform = window.getWortalPlatform();
+    const browser = navigator.userAgent;
+    logEvent("GameStart", {
+        game: GAME_NAME,
+        platform: platform,
+        browser: browser,
+        country: country,
+    });
+    setInterval(function () {
+        if (document.visibilityState !== "hidden") {
+            gameTimer += 1;
+        }
+    }, 1000);
+}
+
+// This is automatically called when the browser is closed. It should not be called by the game.
+function logGameEnd() {
+    logEvent("GameEnd", {
+        game: GAME_NAME,
+        timePlayed: gameTimer,
+    });
+}
+
+// This should not be directly called, instead use the logEvent functions that correspond to the event
+// you want to report.
+function logEvent(name, features) {
+    let request = new XMLHttpRequest();
+    request.open("POST", "https://wombat.digitalwill.co.jp/wortal/events");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify({ name, features }));
+}
+
+function getIntlData() {
+    return fetch('./intl-data.json')
+        .then(response => response.json())
+        .then(data => getPlayerCountry(data))
+        .catch(error => console.log(error));
+}
+
+// This uses the time zone setting of the player to determine their country.
+// We do this to avoid collecting any personal data on the player for GDPR compliance.
+// The location is very coarse and easily spoofed so nothing here can identify the player.
+function getPlayerCountry(data) {
+    if (data == null) {
+        return "Nulltherlands";
+    }
+    const zone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const arr = zone.split("/");
+    const city = arr[arr.length - 1];
+    return data[city];
 }
 
 function removeLoadingCover() {
