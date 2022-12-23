054109ed3bdf6098fe40b4e9070c57743be70ea2
Tim Dillon <tim@itsdits.com>
2022-12-23 18:53:56 +0900

    chore: clean up locale handler

--- HexGL/src/bkcore/hexgl/LocaleHandler.js
+++ HexGL/src/bkcore/hexgl/LocaleHandler.js
@@ -73,125 +73,46 @@ function LowerJSON(json) {
     let newJson = {};
     Object.keys(json).forEach(function (key) {
         let newKey = key;
-        if (typeof key === 'string')
+        if (typeof key === 'string') {
             newKey = key.toLowerCase();
-        if (typeof json[key] === 'string')
+        }
+        if (typeof json[key] === 'string') {
             newJson[newKey] = json[key].toLowerCase();
-        else
+        } else {
             newJson[newKey] = LowerJSON(json[key]);
+        }
     });
     return newJson;
 }
 
-browserLanguage = (window.navigator.userLanguage || window.navigator.language).toLowerCase();
-
-elements = document.getElementsByClassName('string');
-
-translation = LowerJSON(translation);
+let browserLanguage = (window.navigator.userLanguage || window.navigator.language).toLowerCase();
+let elements = document.getElementsByClassName('string');
+let translation = LowerJSON(translation);
 
 function TranslateText() {
-    for (i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         let string = elements[i].innerText.toLowerCase();
         let newString = "";
         if (translation[string]) {
-            if (translation[string][browserLanguage])
+            if (translation[string][browserLanguage]) {
                 newString = translation[string][browserLanguage];
-            else
+            } else {
                 newString = string;
-        }
-        else {
-            sections = string.split(":");
-            for (j = 0; j < sections.length; j++) {
+            }
+        } else {
+            let sections = string.split(":");
+            for (let j = 0; j < sections.length; j++) {
                 sections[j] = sections[j].trim();
-                if (translation[sections[j]])
+                if (translation[sections[j]]) {
                     newString += translation[sections[j]][browserLanguage];
-                else
+                } else {
                     newString += sections[j];
-
-                if (j != sections.length - 1)
+                }
+                if (j !== sections.length - 1) {
                     newString += ": ";
+                }
             }
         }
         elements[i].innerText = newString;
     }
 }
-
-
-
-
-// const test = {
-//     "start": {
-//         "en": "Start",
-//         "ja": "スタート"
-//     },
-//     "controls": {
-//         "en": "Controls",
-//         "ja": "操作"
-//     },
-//     "keyboard": {
-//         "en": "Keyboard",
-//         "ja": "キーボード"
-//     },
-//     "touch": {
-//         "en": "Touch",
-//         "ja": "タッチ"
-//     },
-//     "Leap Motion Controller": {
-//         "en": "Leap Motion Controller",
-//         "ja": "Leap モーションコントローラー"
-//     },
-//     "gamepad": {
-//         "en": "Gamepad",
-//         "ja": "ゲームパッド"
-//     },
-//     "quality": {
-//         "en": "Quality",
-//         "ja": "解像度"
-//     },
-//     "low": {
-//         "en": "Low",
-//         "ja": "低い"
-//     },
-//     "medium": {
-//         "en": "Medium",
-//         "ja": "普通"
-//     },
-//     "high": {
-
-//         "en": "High",
-//         "ja": "高い"
-//     },
-//     "veryhigh": {
-//         "en": "Very High",
-//         "ja": "とても高い"
-//     },
-//     "hud": {
-
-//         "en": "HUD",
-//         "ja": "HUD"
-//     },
-//     "on": {
-//         "en": "On",
-//         "ja": "オン"
-//     },
-//     "off": {
-//         "en": "Off",
-//         "ja": "オフ"
-//     },
-//     "credits": {
-//         "en": "Credits",
-//         "ja": "クレジット"
-//     },
-//     "hexgl": {
-//         "en": "HexGL",
-//         "ja": "HexGL"
-//     },
-//     "hexgldesc": {
-//         "en": "HexGL is a futuristic, fast-paced racing game",
-//         "ja": "HexGLは、未来的でハイペースなレーシングゲームです。"
-//     },
-//     "hexgltutorial": {
-//         "en": "Tap & hold to accelerate\nSwipe to move left and right",
-//         "ja": "タップ長押しで加速\n左右にスワイプして操作"
-//     }
-// }
